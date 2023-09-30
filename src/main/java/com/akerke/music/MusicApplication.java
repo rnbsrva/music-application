1d6815622c636a6dbb58c325c33559124a0292cc
rnbsrva <orynbasarovaakerke88@gmail.com>
2023-09-30 13:43:01 +0600

    hello

--- src/main/java/com/akerke/music/MusicApplication.java
+++ src/main/java/com/akerke/music/MusicApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MusicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MusicApplication.class, args);
+        SpringApplication.run(MusicApplication.class, args); // hello
     }
 
 }
